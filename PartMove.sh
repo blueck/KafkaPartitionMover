57f7553c426f25f203be74dd3d6ad935a48a6da4
Brian <brian@Brians-MacBook-Pro.local>
2016-05-03 17:59:57 -0700

    fix the double loop and checkpoint backup is only run once now

--- PartMove.sh
+++ PartMove.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #This is simple script to help move Kafka partitions from one path to another and also edit the appropriate checkpoint files in each directory.
 
-# Help 
+# Vars
+checkPointFiles="recovery-point-offset-checkpoint replication-offset-checkpoint"
+setRand=$RANDOM
 
+# Help 
 if [[ $# <  6 ]]
 then
   echo "When running this script, please make sure the broker is stopped!"
   echo "-sd|--source-directory         Source directory for partition move"
   echo "-dd|--destination-directory    Destination directory for partition move"
   echo "-p|--partitions                Space seperated list of partitions to move"
-  echo "Example: ./test1.sh -sd /vol1/kafka -dd /vol2/kafka --partitions topic1 topic2"
+  echo "Example: ./PartMove.sh -sd /vol1/kafka -dd /vol2/kafka --partitions topic1-1 topic2-1 topic3-2"
 exit 0
 fi
+
 # Getting opts
 while [[ $# > 1 ]]
 do
@@ -42,7 +46,6 @@ esac
 done
 
 # Confirming opts with user
-
 echo "Input directory: $srcDir"
 echo "Output directory: $destDir"
 echo "Partition(s) to move: $parts"
@@ -57,40 +60,39 @@ case $safe in
   ;;
 esac
 
-# Moving partitions
+# Making backups
+for checkFile in $checkPointFiles
+do
+  cp -p $srcDir/$checkFile /tmp/$checkFile.orig.$( echo $srcDir | sed "s/\//_/g" ).$setRand
+  cp -p $destDir/$checkFile /tmp/$checkFile.orig.$( echo $destDir | sed "s/\//_/g" ).$setRand
+done
 
+# Moving partitions
 for i in $parts
 do
   echo "Copying srcDir/$i"
   cp -pR $srcDir/$i $destDir/ || exit 1
   echo "Updating checkpoint files"
+  
   # Checkpoint file change
-
-  for checkFile in recovery-point-offset-checkpoint replication-offset-checkpoint
+  for checkFile in $checkPointFiles
   do
-    # making backups
-    cp -p $srcDir/$checkFile /tmp/$checkFile.orig.$( echo $srcDir | sed "s/\//_/g" ).$RANDOM
-    cp -p $destDir/$checkFile /tmp/$checkFile.orig.$( echo $destDir | sed "s/\//_/g" ).$RANDOM
-    for i in $parts
-    do
-      chkPntName=$( echo $i | sed 's/\(.*\)-/\1 /' )
-      pCntSrcOrig=$[ $( wc -l < $srcDir/$checkFile ) -2 ]
-      pCntDestOrig=$[ $( wc -l < $destDir/$checkFile ) -2 ]   
-      grep "$chkPntName" $srcDir/$checkFile >> $destDir/$checkFile 
-      if (( $? > 0 ))
-      then 
-         echo "Could not move parition to new checkpoint file, exiting"
-         exit 1
-      fi
-      pCntSrcNew=$[ $( wc -l < $srcDir/$checkFile ) -3 ]
-      sed -e "2s/$pCntSrcOrig/$pCntSrcNew/" -e "/$chkPntName/d" -i.bak $srcDir/$checkFile || exit 1
-      mv $srcDir/$checkFile.bak /tmp/$checkFile.bak.src
-      pCntDestNew=$[ $( wc -l < $destDir/$checkFile ) -2 ]   
-      sed -e "2s/$pCntDestOrig/$pCntDestNew/" -i.bak $destDir/$checkFile || exit 1
-      mv $destDir/$checkFile.bak /tmp/$checkFile.bak.dest
-    done
+    chkPntName=$( echo $i | sed 's/\(.*\)-/\1 /' )
+    pCntSrcOrig=$[ $( wc -l < $srcDir/$checkFile ) -2 ]
+    pCntDestOrig=$[ $( wc -l < $destDir/$checkFile ) -2 ]   
+    grep "$chkPntName" $srcDir/$checkFile >> $destDir/$checkFile 
+    if (( $? > 0 ))
+    then 
+       echo "Could not move parition to new checkpoint file, exiting"
+       exit 1
+    fi
+    pCntSrcNew=$[ $( wc -l < $srcDir/$checkFile ) -3 ]
+    sed -e "2s/$pCntSrcOrig/$pCntSrcNew/" -e "/$chkPntName/d" -i.bak $srcDir/$checkFile || exit 1
+    mv $srcDir/$checkFile.bak /tmp/$checkFile.bak.src
+    pCntDestNew=$[ $( wc -l < $destDir/$checkFile ) -2 ]   
+    sed -e "2s/$pCntDestOrig/$pCntDestNew/" -i.bak $destDir/$checkFile || exit 1
+    mv $destDir/$checkFile.bak /tmp/$checkFile.bak.dest
   done
   echo "Removing $srcDir/$i"
   rm -rf $srcDir/$i
 done
-
